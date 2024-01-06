9b1fdbd8938608495c4bb03d396a8ee19cdd500b
Hendrik Schuster <hendrik.schuster@consileon.de>
2024-01-06 11:53:46 +0100

    Initial commit

---
+++ src/test/java/platform/codingnomads/co/demorecipe/DemoRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package platform.codingnomads.co.demorecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
